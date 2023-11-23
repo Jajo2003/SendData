61c7c143a9e8063764336d66ae419a9c5b44df49
jeiji <jeiji@gmail.com>
2023-11-23 16:43:54 +0400

    Add project files.

---
+++ retrieveData/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Numerics;
+using OfficeOpenXml;
+using System.Net.Http;
+using System.Collections.Generic;
+namespace retrievedata
+{
+	class dataClass
+	{
+		public static async Task Main()
+		{
+			var path = "C:\\Users\\JAJO\\Downloads\\tracking.xlsx";
+			List<string> Trackings = new List<string>();
+
+			try
+			{
+				ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+				using (var package = new ExcelPackage(new FileInfo(path)))
+				{ 
+
+					var sheet = package.Workbook.Worksheets[0];
+
+
+					int rowCount = sheet.Dimension.Rows;
+
+					//Cikli amatebs Excel failshi arsebul informacias.
+					for (int i = 1; i <= rowCount; i++) 
+					{
+						Trackings.Add(sheet.Cells["A" + $"{i}"].Text);						
+					}
+
+
+					await SendData(Trackings);
+					
+				}
+				
+
+
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine($"An error occurred: {ex.Message}");
+			}
+		}
+
+		private static async Task SendData(List<string> Trackings)
+		{
+
+			try
+			{
+				using (var httpClient = new HttpClient())
+				{
+					var Url = "http://localhost:5500/AJAX/";
+
+					var Data = new FormUrlEncodedContent(new Dictionary<string, string>{
+
+						{"Trackings",string.Join(".",Trackings) }
+
+					});
+					var response = await httpClient.PostAsync(Url, Data);
+
+					response.EnsureSuccessStatusCode();
+
+					var responseBody = await response.Content.ReadAsStringAsync();
+					Console.WriteLine($"Response from website : ${responseBody}");
+					Console.WriteLine("Data Retrieved Succesfully");
+
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"{ex.Message}   Error");
+			}
+
+		}
+	}
+
+
+
+
+
+}
